5519750b625d41720b50e09d7bf6eb546fbd8b07
alex.hodes@gmail.com <alex.hodes@gmail.com@6b60c826-1e49-eb6c-ec3f-0332a4cd4343>
2012-11-23 15:05:15 +0000

    Heb de logfile maker mooi gemaakt. En hij doet werken.

--- trunk/3DEngine/TextLog.cpp
+++ trunk/3DEngine/TextLog.cpp
@@ -13,12 +13,8 @@ TextLog::~TextLog(void)
 
 void TextLog::Write(char* argPMessage, char* argPSeverity, char* argPDate, char* argPTime, char* argPFileName, int argLineNumber)
 {
-	//std::ofstream logFile;
+	//File gets created and opened. If the file exists the file will just be opened
 	std::ofstream logFile("Logs//logTest.txt", std::ofstream::app);
-	//logFile.fopen("Logs//logTest.txt", std::ofstream::in | std::ofstream::ate );
-	//char* pBuffer = "";
-	//sprintf( pBuffer, "[%s] [%s] [%s] %s at %s on line %d\n\n", argPDate, argPTime, argSeverity, argPMessage, argPFileName, argLineNumber);
-	//logFile << *pBuffer;
 	logFile << "[" << argPDate << "][" <<  argPTime << "][" << argPSeverity << 
 		"]["<< argPMessage << "]["<< argPFileName << " on line " << argLineNumber <<"]" << std::endl;
 	logFile.close();
