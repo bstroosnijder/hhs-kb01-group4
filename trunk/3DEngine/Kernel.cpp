4c0c6e8abb84b8a4c7c5cc8a61a1f0f12281c85a
steffen_vdv@hotmail.com <steffen_vdv@hotmail.com@6b60c826-1e49-eb6c-ec3f-0332a4cd4343>
2012-11-27 13:52:42 +0000

    Kernel aangepast

--- trunk/3DEngine/Kernel.cpp
+++ trunk/3DEngine/Kernel.cpp
@@ -1,14 +1,16 @@
 #include "Kernel.h"
-#include "WindowManager.h"
-//#include "SceneManager.h"
 
- 
- 
+static Logger textLogger; 
+static Logger consoleLogger;
 engine::Kernel::Kernel(void)
 {
 	WindowManager* windowManager = new WindowManager();
 	//SceneManager* sceneManager = new SceneManager();
-	
+	LogStrategy* textStrat = new TextLog();
+	LogStrategy* consoleStrat = new ConsoleLog();
+
+	textLogger.AddStrategy(textStrat);
+	consoleLogger.AddStrategy(consoleStrat);
 
 }
 
--- trunk/3DEngine/Kernel.h
+++ trunk/3DEngine/Kernel.h
@@ -1,6 +1,11 @@
 //#ifndef KERNEL_H_
 #define KERNEL_H_
 #include "stdlib.h"
+#include "WindowManager.h"
+#include "Logger.h"
+#include "TextLog.h"
+#include "ConsoleLog.h" 
+//#include "SceneManager.h"
 
 namespace engine {  
 class Kernel
