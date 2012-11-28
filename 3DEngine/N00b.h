ad138328dc131a1e06e9a139b4120c9d51e1b856
bas.stroosnijder <bas.stroosnijder@gmail.com>
2012-11-28 08:48:53 +0000

    added the N00b and Sandbox interfaces

---
+++ 3DEngine/N00b.h
@@ -0,0 +1,14 @@
+#ifndef N00b_H_
+#define N00b_H_
+
+namespace engine
+{
+	class N00b
+	{
+	private:
+
+	public:
+	}
+}
+
+#endif
---
+++ 3DEngine/Sandbox.h
@@ -0,0 +1,13 @@
+#ifndef Sandbox_H_
+#define Sandbox_H_
+
+namespace engine
+{
+	class Sandbox
+	{
+	private:
+	public:
+	}
+}
+
+#endif
