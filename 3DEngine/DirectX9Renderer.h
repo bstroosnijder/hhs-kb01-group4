b45cd9ac3d8d28234b3cb1a2c47f946efbceab60
steffen_vdv <steffen_vdv@hotmail.com>
2012-11-30 15:30:05 +0000

    [SvdV] DirectX9Renderer [30/11/2012]

--- 3DEngine/DirectX9Renderer.h
+++ 3DEngine/DirectX9Renderer.h
@@ -1,3 +1,6 @@
+#ifndef DIRECTX9RENDERER_H_
+#define DIRECTX9RENDERER_H_
+
 #include "Renderer.h"
 #include <d3dx9.h>
 
@@ -8,13 +11,24 @@ namespace engine
 	 */
 	class DirectX9Renderer : public Renderer
 	{
+	private:
+		LPDIRECT3D9 pDirect3d;
+		LPDIRECT3DDEVICE9 pDevice;
+		LPDIRECT3DVERTEXBUFFER9 pVertexBuffer;
+		D3DPRESENT_PARAMETERS presentParameters;
 	public:
-		DirectX9Renderer();
+		DirectX9Renderer(HWND argHWin);
 		~DirectX9Renderer();
 
+		void CleanUp();
 		void Clear();
 		void BeginScene();
 		void EndScene();
 		void Present(Window* argWindow);
+		void DrawPrimitive();
+		void SetFVF();
+		void SetStreamSource();
 	};
 }
+
+#endif
