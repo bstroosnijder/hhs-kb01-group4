0e3d2138bf88a62931c7dfa424b8fa8b9ba07b40
steffen_vdv <steffen_vdv@hotmail.com>
2012-11-30 14:02:09 +0000

    [SvdV] Achterstallige commit [30/11/2012]

---
+++ 3DEngine/DirectX9Renderer.cpp
@@ -0,0 +1,41 @@
+#include "DirectX9Renderer.h"
+
+namespace engine
+{
+	/**
+	 * Clears the screen.
+	 * TODO more info
+	 */
+	void DirectX9Renderer::Clear()
+	{
+
+	}
+
+	/**
+	 * Begins a scene.
+	 * TODO more info
+	 */
+	void DirectX9Renderer::BeginScene()
+	{
+
+	}
+
+	/**
+	 * Ends a scene.
+	 * TODO more info
+	 */
+	void DirectX9Renderer::EndScene()
+	{
+
+	}
+
+	/**
+	 * Presents stuff to the window.
+	 * @param	window		The window to present the drawn data to.
+	 * TODO more info
+	 */
+	void DirectX9Renderer::Present(Window* argWindow)
+	{
+
+	} 
+}
