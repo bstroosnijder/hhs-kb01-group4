795b8be3ff07d8018bbb9b55481e717fd97a5f50
alex.hodes@gmail.com <alex.hodes@gmail.com@6b60c826-1e49-eb6c-ec3f-0332a4cd4343>
2012-12-03 13:10:05 +0000

    AH: entiet interface gemaakt een map entities toegevoegd. De camera klasse aanbemaakt en aan begonnen

--- trunk/3DEngine/Entity.h
+++ trunk/3DEngine/Entity.h
@@ -7,14 +7,21 @@ namespace engine
 	/**
 	 * The Entity class is a representation of an object in a game or program.
 	 * This class is responsible for drawing and updating itself on the screen.
+	 * Class is abstract becuase entities will created via this template
+	 * @author Alex Hodes
 	 */
 	class Entity
 	{
 	public:
-		Entity();
-		~Entity();
-		void Draw(Renderer* argPRenderer);
-		void Update();
+		virtual void Draw(Renderer* argPRenderer) =0;
+		//this method is not in the class diagram.
+		virtual void Update()=0;
+	private:
+		int x;
+		int y;
+		int z; 
+		//maybe a position matrix might be nessacery
+
 	};
 }
 
