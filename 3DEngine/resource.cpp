9916f5c7b13907347aef2104993e14a42d30b228
steffen_vdv <steffen_vdv@hotmail.com>
2012-12-07 12:49:35 +0000

    AH: Tip bij updaten sluit de solution.
    De resource klasse toegevoegd.

---
+++ 3DEngine/resource.cpp
@@ -0,0 +1,19 @@
+#include "resource.h"
+
+/**
+ * This does nothing
+ * todo
+ * @author Alex Hodes
+ */
+resource::resource(void)
+{
+}
+
+/**
+ * This does nothing
+ * todo
+ * @author Alex Hodes
+ */
+resource::~resource(void)
+{
+}
