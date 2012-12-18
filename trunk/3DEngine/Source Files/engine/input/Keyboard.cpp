97031310a3c594d559b11045013543f20c75284f
alex.hodes@gmail.com <alex.hodes@gmail.com@6b60c826-1e49-eb6c-ec3f-0332a4cd4343>
2012-12-18 14:04:15 +0000

    [Alex] Tip: sluit solution! Heb de mouse en keyboard klasse aangemaakt. En deze in de input map en filter gezet! ook heb ik de input manager aangepast. Tevens heb ik wat extra libs toegevoegd

---
+++ trunk/3DEngine/Source Files/engine/input/Keyboard.cpp
@@ -0,0 +1,53 @@
+#include "..\..\..\Header Files\engine\input\Keyboard.h"
+//no clue why we need this
+#define KEYDOWN(name, key) (name[key] & 0x80) 
+
+	/**
+	 * todo
+	 * @author Alex Hodes
+	 */
+engine::Keyboard::Keyboard()
+{
+}
+
+	/**
+	 * todo
+	 * @author Alex Hodes
+	 */
+engine::Keyboard::~Keyboard()
+{
+}
+
+	/**
+	 * todo
+	 * @author Alex Hodes
+	 */
+//engine::Keyboard::Keyboard()
+//{
+//}
+
+	/**
+	 * todo
+	 * @author Alex Hodes
+	 */
+bool engine::Keyboard::InitKeyboard()
+{
+	return true;
+}
+	/**
+	 * todo
+	 * @author Alex Hodes
+	 */
+void engine::Keyboard::SaveReleaseDevice()
+{
+
+}
+
+	/**
+	 * todo
+	 * @author Alex Hodes
+	 */
+bool engine::Keyboard::DoAcquire()
+{
+	return true;
+}
