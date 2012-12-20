1baf519c8f92a0b48e5f16e1530ebadcdf8a634b
bas.stroosnijder@gmail.com <bas.stroosnijder@gmail.com@6b60c826-1e49-eb6c-ec3f-0332a4cd4343>
2012-12-20 12:23:11 +0000

    bitmap loading fixed

--- trunk/3DEngine/Header Files/engine/utils/Bitmap.h
+++ trunk/3DEngine/Header Files/engine/utils/Bitmap.h
@@ -3,33 +3,32 @@
 
 #include <iostream>
 #include <fstream>
-#include <windows.h>
 #include <ios>
 #include "..\logger\Logger.h"
 
 namespace engine
 {
 	typedef struct tagBITMAPFILEHEADER {
-	  WORD  bfType;
-	  DWORD bfSize;
-	  WORD  bfReserved1;
-	  WORD  bfReserved2;
-	  DWORD bfOffBits;
-	} BITMAPFILEHEADER, *PBITMAPFILEHEADER;
+	  unsigned short bfType;
+	  unsigned long bfSize;
+	  unsigned short  bfReserved1;
+	  unsigned short  bfReserved2;
+	  unsigned long bfOffBits;
+	} BITMAPFILEHEADER;
 
 	typedef struct tagBITMAPINFOHEADER {
-		DWORD biSize;
-		LONG  biWidth;
-		LONG  biHeight;
-		WORD  biPlanes;
-		WORD  biBitCount;
-		DWORD biCompression;
-		DWORD biSizeImage;
-		LONG  biXPelsPerMeter;
-		LONG  biYPelsPerMeter;
-		DWORD biClrUsed;
-		DWORD biClrImportant;
-	} BITMAPINFOHEADER, *PBITMAPINFOHEADER;
+		unsigned long biSize;
+		long  biWidth;
+		long  biHeight;
+		unsigned short  biPlanes;
+		unsigned short  biBitCount;
+		unsigned long biCompression;
+		unsigned long biSizeImage;
+		long  biXPelsPerMeter;
+		long  biYPelsPerMeter;
+		unsigned long biClrUsed;
+		unsigned long biClrImportant;
+	} BITMAPINFOHEADER;
 
 	class Bitmap
 	{
